9ef5d9c216c2e4a2f4eef92a2aab9d1bdff9fb41
Thales Xavier <thalescaceres@gmail.com>
2024-08-05 22:02:07 -0300

    adicionando verificações

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const cpfInput = document.querySelector("#cpf");
 const cnpjInput = document.querySelector("#cnpj");
 const cepInput = document.querySelector("#cep");
 const addressInput = document.querySelector("#address");
+const neighborhoodInput = document.querySelector("#neighborhood");
+const cityInput = document.querySelector("#city");
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -25,9 +27,57 @@ form.addEventListener("submit", (event) => {
         return;
     }
 
-    
+    if (cpfInput.value === "" && cnpjInput.value === "") {
+        alert("CPF ou CNPJ não pode ser vazio");
+        return;
+    }
+
+    if (cepInput.value === "") {
+        alert("CEP não pode ser vazio");
+        return;
+    }
+
+    if (addressInput.value === "") {
+        alert("Endereço não pode ser vazio");
+        return;
+    }
+
+    if (neighborhoodInput.value === "") {
+        alert("Bairro não pode ser vazio");
+        return;
+    }
+
+    if (cityInput.value === "") {
+        alert("Cidade não pode ser vazio");
+        return;
+    }
+
 
     form.submit();
+});
+
+cpfInput.addEventListener('keypress', () => {
+    if (cpfInput.value.length === 3 || cpfInput.value.length === 7) {
+        cpfInput.value += ".";
+    }
+
+    if (cpfInput.value.length === 11) {
+        cpfInput.value += "-";
+    }
+});
+
+cnpjInput.addEventListener('keypress', () => {
+    if (cnpjInput.value.length === 2 || cnpjInput.value.length === 6) {
+        cnpjInput.value += ".";
+    }
+
+    if (cnpjInput.value.length === 10) {
+        cnpjInput.value += "/";
+    }
+
+    if (cnpjInput.value.length === 15) {
+        cnpjInput.value += "-";
+    }
 });
 
 function isEmailValid(email) {
